955a5532acb8ba67cb0971baaad25a632b885f4a
klarok <karaylu@gmail.com>
2018-11-22 12:32:28 -0800

    Enemies move to new positions every second.
    	- Removed border from .board in CSS

--- client/watchout.js
+++ client/watchout.js
@@ -13,41 +13,63 @@ let svg = board.append('svg')
 		.attr('height', 400)
 		.style('background-color', '#fff');
 
-let enemies = d3.range(10).map(i => {
-	return { 
-		i: i,
-		r: random(15, 10),
-		// x: random(svg[0][0].clientWidth),
-		// y: random(svg[0][0].clientHeight),
-		fill: `rgb(${random(255)}, ${random(255)}, ${random(255)})`
-	}
-});
 
-function random(max, min) {
-	return (min !== undefined) ? Math.floor(Math.random() * (max - min) + min) : Math.floor(Math.random() * max);
-}
+/////// ENEMIES ///////////////////////////////////////
 
-function spawn(data) {
+let generateEnemyData = function(num) {
+	return d3.range(num).map(i => {
+		return { 
+			i: i,
+			r: random(15, 10),
+			fill: randomColor(undefined, 70)
+		}
+	});
+};
+
+let spawn = function(data) {
 	let circle = svg.selectAll('circle')
 		.data(data);
 
 	circle = circle.enter().append('circle')
+			.classed('enemy', true)
 			.attr('fill', d => d.fill)
 			.attr('r', d => d.r);
-			// .attr('cx', d => d.x)
-			// .attr('cy', d => d.y);
 	circle.each(function() {
-		updateLocation(d3.select(this));
-		// d3.select(this)
-		// 	.attr('cx', random(svg[0][0].clientWidth))
-		// 	.attr('cy', random(svg[0][0].clientHeight));
+		generateLocation(d3.select(this));
 	});
+
+	return circle;
 }
 
-function updateLocation(selection) {
+let generateLocation = function(selection) {
+	let x = [svg[0][0].clientWidth - selection.attr('r'), selection.attr('r')];
+	let y = [svg[0][0].clientHeight - selection.attr('r'), selection.attr('r')];
 	selection
-			.attr('cx', random(svg[0][0].clientWidth))
-			.attr('cy', random(svg[0][0].clientHeight));
+			.attr('cx', random(x[0], x[1]))
+			.attr('cy', random(y[0], y[1]));
 }
 
-spawn(enemies);
+//TODO: keep enemies from falling off the board
+let mobilize = function(selection) {
+	selection
+		.transition()
+			.duration(1000)
+			.attr('cx', () => random(svg[0][0].clientWidth))
+			.attr('cy', () => random(svg[0][0].clientHeight));
+}
+
+/////// HELPERS ///////////////////////////////////////
+
+let random = function(max, min) {
+	return (min !== undefined) ? Math.floor(Math.random() * (max - min) + min) : Math.floor(Math.random() * max);
+}
+let randomColor = function(max = 255, min) {
+	return `rgb(${random(255, min)}, ${random(255, min)}, ${random(255, min)})`;
+}
+
+
+/////// MAIN ///////////////////////////////////////
+
+let enemyData = generateEnemyData(10);
+let enemies = spawn(enemyData);
+setInterval(() => mobilize(enemies), 1000);
